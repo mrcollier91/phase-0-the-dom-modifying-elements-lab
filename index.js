8542e0c24eaaaaab1cc1eb52737ec0cc1185306b
mrcollier91 <colliermatt91@gmail.com>
2022-06-01 13:39:53 -0500

    commit tree c30b668ee55549a81ac5eee4f26442cbd2f06a5c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement('h1')
+//document.body.append(newHeader)
+//newHeader.setAttribute('id', 'victory')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Matt is the champion'
